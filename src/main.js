fe9d4a8ca83bc329eca86cd9fbf6dc6a1e00fd4f
tian996 <1714925579@qq.com>
2020-05-07 13:35:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUi)
 Vue.config.productionTip = false
 
 new Vue({
