0b85b0df4f9d34fb82e0656b437383917be148da
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-05-07 12:04:31 +0800

    Update package-lock.json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/my">My</router-link>
-    </div>
+
     <router-view/>
   </div>
 </template>
@@ -30,4 +26,8 @@
     }
   }
 }
+  *{
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- src/components/zh/MyHeader.vue
+++ src/components/zh/MyHeader.vue
@@ -1,13 +1,90 @@
 <template>
-    <div></div>
+    <div>
+<!--        登录背景、登录按钮-->
+        <div class="headerbg">
+            <div class="login"></div>
+            <div class="clicklogin" @click="login">点击登录/注册</div>
+        </div>
+<!--        代金券、红包、钱包-->
+        <div class="mybag">
+            <div>
+                <img src="../../../public/img/zh/daijinquan.png" alt="">
+                <div>代金券</div>
+            </div>
+            <div>
+                <img src="../../../public/img/zh/redpack.png" alt="">
+                <div>红包</div>
+            </div>
+            <div>
+                <img src="../../../public/img/zh/pockt.png" alt="">
+                <div>钱包</div>
+            </div>
+
+        </div>
+<!--        开会员广告-->
+        <img src="../../../public/img/zh/member.png" alt="" class="member" @click="member">
+    </div>
 </template>
 
 <script>
     export default {
-        name: "MyHeader"
+        name: "MyHeader",
+        methods:{
+            // 登录
+            login(){
+                console.log(111)
+            },
+            // 充会员
+            member(){
+                console.log(222)
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+    /*圆角*/
+    @br:4px;
+    /*登录背景、按钮*/
+    .headerbg{
+        overflow: hidden;
+        width: 100%;
+        height: 262px;
+        background-image: url('../../../public/img/zh/bgimg.png');
+        background-size: cover;
+        z-index: 5;
+    }
+    .login{
+        width: 64px;
+        height: 64px;
+        border-radius: 50%;
+        background-color: white;
+        box-shadow: 0px 0px 5px 5px rgba(235, 235, 235, 0.4);
+        position: absolute;
+        left: 148px;
+        top: 60px;
+    }
+    .clicklogin{
+        color: white;
+        margin-top: 143px;
+        margin-left: -10px;
+        font-size: 16px;
+    }
+    /*代金券、红包、钱包*/
+    .mybag{
+        background-color: white;
+        z-index: -1;
+        width: 334px;
+       margin: -5px auto;
+        display: flex;
+        justify-content: space-around;
+        border-radius: @br;
+        &>div{
+            margin: 10px 0;
+        }
+    }
+    /*开会员广告*/
+    .member{
+        margin: 14px 0;
+    }
 </style>
