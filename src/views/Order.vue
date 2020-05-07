13947a57aeaf6b6f591c02f62b50ab07de3bb52d
tian996 <1714925579@qq.com>
2020-05-07 13:39:58 +0800

    Create Order.vue

---
+++ src/views/Order.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <category></category>
+        <TableBar></TableBar>
+    </div>
+</template>
+
+<script>
+    import TableBar from '../components/TableBar.vue'
+    import category from '../components/mxm/category.vue'
+
+    export default {
+        name: "Order",
+        components:{
+            TableBar,
+            category
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
