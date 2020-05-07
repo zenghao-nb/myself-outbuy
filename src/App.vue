dfa6f3e60ee8dbaa1ffcc898dd6b531db87361d7
zenghao-nb <59545524+zenghao-nb@users.noreply.github.com>
2020-05-07 09:16:49 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-
-
-
       <router-link to="/about">About</router-link>
       <router-link to="/dianpu">Dianpu</router-link>
-
-      <router-link to="/about">About</router-link>|
-      <router-link to="/my">My</router-link>
-
-
-      <router-link to="/about">About</router-link>|
-      <router-link to="/my">My</router-link>
-
-
-      <router-link to="/about">About</router-link>|
       <router-link to="/my">My</router-link>
 
     </div>
