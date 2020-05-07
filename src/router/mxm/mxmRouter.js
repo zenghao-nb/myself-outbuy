f9f01ad4d2bb96c68d3abeee1cd010e9a9822272
tian996 <1714925579@qq.com>
2020-05-07 18:29:42 +0800

    Update mxmRouter.js

--- src/router/mxm/mxmRouter.js
+++ src/router/mxm/mxmRouter.js
@@ -7,5 +7,19 @@ export  default [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../../views/Order.vue')
+    },  {
+        path: '/detail',
+        name: 'detail',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../../views/mxm/detail.vue')
+    },{
+        path: '/',
+        name: 'detail',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../../views/mxm/detail.vue')
     },
 ]
