5fc7bc15b06ed704de21cd04a79be888792b15e3
tian996 <1714925579@qq.com>
2020-05-07 13:39:31 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+<<<<<<< HEAD
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+=======
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUi)
+>>>>>>> mxm-branch
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
